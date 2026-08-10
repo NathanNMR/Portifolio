2f9f6dc4b6bfbb854ed40dbf7fff56d295695390
Nathan M. Ramos <82791814+NathanNMR@users.noreply.github.com>
2026-08-09 22:40:46 -0300

    Refactor DB_CONFIG to use fixed port and SSL setting
    Updated DB_CONFIG to set port directly and added ssl_disabled key.

--- app.py
+++ app.py
@@ -72,16 +72,15 @@ def sanitizar_html(texto):
     )
 
 
+# Configuração blindada com porta garantida como inteiro (int)
 DB_CONFIG = {
     "host": os.environ.get("DB_HOST", "mysql-203b7745-nathannmr.d.aivencloud.com"),
-    "port": int(os.environ.get("DB_PORT", 20110)),
+    "port": 20110,
     "user": os.environ.get("DB_USER", "avnadmin"),
     "password": os.environ.get("DB_PASSWORD"),
     "database": os.environ.get("DB_NAME", "defaultdb"),
+    "ssl_disabled": False,
 }
-
-if DB_CONFIG["host"] != "localhost":
-    DB_CONFIG["ssl_disabled"] = False
 
 if not DB_CONFIG["password"]:
     raise RuntimeError(
